3066518a3746cb79c456a7369a750bde36097002
Andre Passos Braga <andre.passos.braga@kyndryl.com>
2025-02-15 23:57:53 -0300

    remove scalable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      viewport:
+        'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
     },
   },
   compatibilityDate: '2024-11-01',
